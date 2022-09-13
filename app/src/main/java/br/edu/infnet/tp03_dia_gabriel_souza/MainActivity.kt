f27e98389532406b00b036380e4f22e5832e4802
gabriel.gsouza <gabriel@mpsouza.com.br>
2022-09-12 23:34:09 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/infnet/tp03_dia_gabriel_souza/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.edu.infnet.tp03_dia_gabriel_souza
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linear_layout)
+    }
+}
